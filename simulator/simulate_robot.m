493478d789a00eb3c821b3cec206a015fd68151b
Jonny Cohen <jonnycohen16@gmail.com>
2024-11-04 15:56:49 -0500

    okay working better

--- simulator/simulate_robot.m
+++ simulator/simulate_robot.m
@@ -15,27 +15,16 @@ function simulate_robot()
     l_B_m_body = l_body / 2;
 
     % Motor and gravity parameters
-    N = 18.75; Ir = 0.0035 / N^2; g = 3;
+    N = 18.75; Ir = 0.0035 / N^2; g = 9.8;
 
     % Updated parameter vector to match all defined parameters in derive_robot.m
     p = [m1 m2 m3 m4 m5 m6 m7 m8 m_body I1 I2 I3 I4 I5 I6 I7 I8 I_body Ir N ...
          l_O_m1 l_B_m2 l_A_m3 l_C_m4 l_B_m_body l_OA l_OB l_AC l_DE l_body g]';
 
     %% Initial Conditions
-    x_base = 0; y_base = .2; th1 = -pi/4; th2 = pi/2; th3 = -pi/4; th4 = pi/2; th5 = 0;
+    x_base = 0; y_base = .5; th1 = -pi/4; th2 = pi/2; th3 = -pi/4; th4 = pi/2; th5 = 0;
     dx_base = 0; dy_base = 0; dth1 = 0; dth2 = 0; dth3 = 0; dth4 = 0; dth5 = 0;
-    
-    % Define trajectory parameters for left and right legs
-    p_traj_left.omega = .0003;
-    p_traj_left.x_0   = -0.02;   % Center of ellipse for the left foot, relative to body
-    p_traj_left.y_0   = -0.125;
-    p_traj_left.r     = 0.025;   % Radius of ellipse for the left foot
-
-    p_traj_right.omega = -.0003;
-    p_traj_right.x_0   = 0.02;   % Center of ellipse for the right foot, relative to body
-    p_traj_right.y_0   = -0.125;
-    p_traj_right.r     = 0.025;  % Radius of ellipse for the right foot
-
+    initial_angles = [th1; th2; th3; th4];
     %% Simulation Parameters
     dt = 0.001; tf = 1; tspan = 0:dt:tf;
     num_steps = length(tspan);
@@ -46,7 +35,8 @@ function simulate_robot()
         % Calculate control torques for both legs
         t = tspan(i);
         %tau = control_law(t, z_out(:,i), p, p_traj_left, p_traj_right);
-        tau = [0;0;0;0];
+        %tau = [0;0;0;0];
+        tau = control_law(t, z_out(:,i), p, initial_angles);
         % Compute dynamics
         dz = dynamics(t, z_out(:, i), p, tau);
         
@@ -118,9 +108,9 @@ function [Fc_left, Fc_right] = contact_forces(z, p)
 
     % Parameters for ground contact
     ground_level = 0;
-    Kc = 1000;  % Spring constant for contact
+    Kc = 100;  % Spring constant for contact
     Dc = 20;    % Damping coefficient for contact
-    mu = 0.8;   % Friction coefficient
+    mu = 1;   % Friction coefficient
 
     % Initialize contact forces (horizontal and vertical components)
     Fc_left = [0; 0];
@@ -140,6 +130,7 @@ function [Fc_left, Fc_right] = contact_forces(z, p)
         else                     % Static friction condition (no sliding)
             Fx_right = -min(mu * Fy_right, abs(Kc * Vx_right)) * sign(Vx_right);
         end
+        Fx_right = 0;
 
         % Combine into total contact force for right foot
         Fc_right = [Fx_right; Fy_right];
@@ -159,86 +150,50 @@ function [Fc_left, Fc_right] = contact_forces(z, p)
         else                     % Static friction condition (no sliding)
             Fx_left = -min(mu * Fy_left, abs(Kc * Vx_left)) * sign(Vx_left);
         end
+        Fx_left = 0;
 
         % Combine into total contact force for left foot
         Fc_left = [Fx_left; Fy_left];
     end
 end
 
+function tau = control_law(t, z, p, initial_angles)
+    % Define passive torsional spring and damper parameters
+    K_torsional = 1;  % Torsional spring constant
+    D_torsional = .1;    % Damping constant
+    max_torque = 1;   % Max torque limit to prevent instability
 
+    % Extract joint angles and angular velocities
+    th1 = z(3);  % Left hip joint
+    th2 = z(4);  % Left knee joint
+    th3 = z(5);  % Right hip joint
+    th4 = z(6);  % Right knee joint
+    dth1 = z(10);  % Left hip joint velocity
+    dth2 = z(11);  % Left knee joint velocity
+    dth3 = z(12);  % Right hip joint velocity
+    dth4 = z(13);  % Right knee joint velocity
 
-function tau = control_law(t, z, p, p_traj_left, p_traj_right)
-    % Controller gains for both legs
-    K_x = 150; % Spring stiffness X
-    K_y = 150; % Spring stiffness Y
-    D_x = 10;  % Damping X
-    D_y = 10;  % Damping Y
+    % Reference (initial) angles from the initial state
+    th1_ref = initial_angles(1);
+    th2_ref = initial_angles(2);
+    th3_ref = initial_angles(3);
+    th4_ref = initial_angles(4);
 
-    % Define stiffness and damping matrices
-    K = diag([K_x, K_y]);
-    D = diag([D_x, D_y]);
+    % Passive torques for each joint (torsional spring-damper system around initial positions)
+    tau1 = -K_torsional * (th1 - th1_ref) - D_torsional * dth1;  % Left hip
+    tau2 = -K_torsional * (th2 - th2_ref) - D_torsional * dth2;  % Left knee
+    tau3 = -K_torsional * (th3 - th3_ref) - D_torsional * dth3;  % Right hip
+    tau4 = -K_torsional * (th4 - th4_ref) - D_torsional * dth4;  % Right knee
 
-    %% Left Leg Control
-    % Desired position, velocity, and acceleration of the left foot
-    omega_swing_left = p_traj_left.omega;
-    rEd_left = [p_traj_left.x_0; p_traj_left.y_0; 0] + ...
-               p_traj_left.r * [cos(omega_swing_left * t); sin(omega_swing_left * t); 0];
-    vEd_left = p_traj_left.r * [-sin(omega_swing_left * t) * omega_swing_left; ...
-                                 cos(omega_swing_left * t) * omega_swing_left; 0];
-    aEd_left = p_traj_left.r * [-cos(omega_swing_left * t) * omega_swing_left^2; ...
-                                -sin(omega_swing_left * t) * omega_swing_left^2; 0];
+    % Clamp torques to avoid unrealistic values
+    tau1 = max(-max_torque, min(tau1, max_torque));
+    tau2 = max(-max_torque, min(tau2, max_torque));
+    tau3 = max(-max_torque, min(tau3, max_torque));
+    tau4 = max(-max_torque, min(tau4, max_torque));
 
-    % Actual position and velocity of the left foot
-    rE_left = position_left_foot(z, p);
-    vE_left = velocity_left_foot(z, p);
-
-    % Compute virtual force for left leg
-    f_left = [K_x * (rEd_left(1) - rE_left(1)) + D_x * (vEd_left(1) - vE_left(1)); ...
-              K_y * (rEd_left(2) - rE_left(2)) + D_y * (vEd_left(2) - vE_left(2))];
-
-    % Map to joint torques for left leg
-    J_left = jacobian_left_foot(z, p);
-    M_left = A_leg(z, p);
-    lambda_left = inv(J_left * inv(M_left) * J_left');
-    mu_left = Corr_leg(z, p);
-    sigma_left = Grav_leg(z, p);
-    f_left = [lambda_left(1,1)*(aEd_left(1) + K_x * (rEd_left(1) - rE_left(1)) + D_x * (vEd_left(1) - vE_left(1))) + mu_left(1) + sigma_left(1);
-              lambda_left(2,2)*(aEd_left(2) + K_y * (rEd_left(2) - rE_left(2)) + D_y * (vEd_left(2) - vE_left(2))) + mu_left(2) + sigma_left(2)];
-    tau_left = J_left' * f_left;
-
-    %% Right Leg Control
-    % Desired position, velocity, and acceleration of the right foot
-    omega_swing_right = p_traj_right.omega;
-    rEd_right = [p_traj_right.x_0; p_traj_right.y_0; 0] + ...
-                p_traj_right.r * [cos(omega_swing_right * t); sin(omega_swing_right * t); 0];
-    vEd_right = p_traj_right.r * [-sin(omega_swing_right * t) * omega_swing_right; ...
-                                   cos(omega_swing_right * t) * omega_swing_right; 0];
-    aEd_right = p_traj_right.r * [-cos(omega_swing_right * t) * omega_swing_right^2; ...
-                                  -sin(omega_swing_right * t) * omega_swing_right^2; 0];
-
-    % Actual position and velocity of the right foot
-    rE_right = position_right_foot(z, p);
-    vE_right = velocity_right_foot(z, p);
-
-    % Compute virtual force for right leg
-    f_right = [K_x * (rEd_right(1) - rE_right(1)) + D_x * (vEd_right(1) - vE_right(1)); ...
-               K_y * (rEd_right(2) - rE_right(2)) + D_y * (vEd_right(2) - vE_right(2))];
-
-    % Map to joint torques for right leg
-    J_right = jacobian_right_foot(z, p);
-    M_right = A_leg(z, p);
-    lambda_right = inv(J_right * inv(M_right) * J_right');
-    mu_right = Corr_leg(z, p);
-    sigma_right = Grav_leg(z, p);
-    f_right = [lambda_right(1,1)*(aEd_right(1) + K_x * (rEd_right(1) - rE_right(1)) + D_x * (vEd_right(1) - vE_right(1))) + mu_right(1) + sigma_right(1);
-               lambda_right(2,2)*(aEd_right(2) + K_y * (rEd_right(2) - rE_right(2)) + D_y * (vEd_right(2) - vE_right(2))) + mu_right(2) + sigma_right(2)];
-    tau_right = J_right' * f_right;
-
-    %% Combine Torques
-    % Concatenate torques for both legs
-    tau = [tau_left; tau_right];
+    % Combine torques into a single vector
+    tau = [tau1; tau2; tau3; tau4]
 end
-
 
 function animate_robot(tspan, z_out, p)
     figure(1); clf; hold on;
